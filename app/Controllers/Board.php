288997cff40ab149b6fbde0c23adad376cf037bb
beautynetkorea <jmh@beautynetkorea.com>
2023-09-12 17:50:43 +0900

    board 수정

--- app/Controllers/Board.php
+++ app/Controllers/Board.php
@@ -7,8 +7,8 @@ use App\Models\NoticeModel;
 
 class Board extends BaseController {
     public function __construct() {
-        $this->notice = new NoticeModel();
-        $this->board = new BoardModel();
+      $this->notice = new NoticeModel();
+      $this->board = new BoardModel();
     }
 
     public function index() {
@@ -16,17 +16,12 @@ class Board extends BaseController {
     }
 
     public function getBoard($type_idx, $idx) {
-        // echo $type_idx;
-        // echo $idx;
-        // print_r ($this->request->uri->getSegments());
-        // var_dump($this->request->uri->getSegments());
-        // echo '1 '.$this->request->uri->getSegment(0).'<br/></br/>';
-        $this->data['board'] = $this->board($type_idx, $idx);
-        return $this->basicLayout('/dash/board', $this->data);
+      $this->data['board'] = $this->board($type_idx, $idx);
+      return $this->basicLayout('/dash/board', $this->data);
     }
 
     public function board($type_idx, $idx) {
-        return $this->board->readBoard($type_idx, $idx)->getResultArray();
+      return $this->board->readBoard($type_idx, $idx)->getResultArray();
     }
 }
 ?>
--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,3 +1,18 @@
-<footer></footer>
+<footer class='mt-5 bg-opacity-10 bg-secondary p-5'>
+  <div class='w-60 m-auto d-flex flex-row'>
+    <div class='w-25 d-flex flex-column'>
+      <p class='fw-bold text-uppercase mb-0'>address</p>
+      <p class='mb-0 font-size-9 text-black-50'>Beautynetkorea Bldg 21, Janggogae-ro 231beonan-gil, Seo-gu, Incheon, Korea</p>
+    </div>
+    <div class='w-25 d-flex flex-column font-size-7'>
+      <a class='text-decoration-none text-uppercase text-black-50 fw-bold' href='/board/getboard/1/2'>Privacy policy</a>
+      <a class='text-decoration-none text-uppercase text-black-50 fw-bold' href='#'>Guide</a>
+    </div>
+    <div class='w-25 d-flex flex-column'>
+      <p class='fw-bold text-uppercase mb-0'>contact us</p>
+      <a class='text-decoration-none text-black-50 font-size-9' href='mailto:mlee5971@beautynetkorea.com' target='_blank'>mlee5971@beautynetkorea.com</a>
+    </div>
+  </div> 
+</footer>
 </body>
 </html>
